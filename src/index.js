61ee8f22795ade22a215d4366c274583fcec0079
roshansourav <roshansouravj@gmail.com>
2021-05-21 01:37:18 +0530

    initial commit for calculator-react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
+    <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class="bg bg3"></div>
+    <div class="content"></div>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+);
